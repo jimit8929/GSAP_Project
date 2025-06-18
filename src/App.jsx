d943402c2e0c68263b9ce672b67de4803837e4e2
jimit8929 <jimit8929@gmail.com>
2025-06-18 21:13:37 +0530

    Clone Creation of Awwwards Website with GSAP

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import NavBar from "./components/NavBar";
+import HeroSection from "./Sections/HeroSection";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+import MessageSection from "./Sections/MessageSection";
+import FlavorSection from "./Sections/FlavorSection";
+import { useGSAP } from "@gsap/react";
+import NutritionSection from "./Sections/NutritionSection";
+import BenefitSection from "./Sections/BenefitSection";
+import TestimonialSection from "./Sections/TestimonialSection";
+import FooterSection from "./Sections/FooterSection";
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 3,
+      effects: true,
+    });
+  });
+
+  return (
+    <main>
+      <NavBar />
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <NutritionSection />
+
+          <div>
+            <BenefitSection />
+            <TestimonialSection />
+          </div>
+
+          <FooterSection />
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default App;

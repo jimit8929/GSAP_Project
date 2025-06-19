0693231535eb14034072577d3fdd11873aa422e0
jimit8929 <jimit8929@gmail.com>
2025-06-19 17:30:45 +0530

    Changes For deploment

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ const NavBar = () => {
   return (
     <nav className="fixed top-0 left-0 z-50 md:p-9 p-3">
       <img
-        src="dist\images\nav-logo.svg"
+        src="/images/nav-logo.svg"
         alt="nav-logo"
         className="md:w-24 w-20"
       />
